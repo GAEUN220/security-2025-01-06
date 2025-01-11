53f6a4ce85efb4d3004ca0fc63181bebd846946c
gaeun220 <gaeun2701@gmail.com>
2025-01-11 19:50:19 +0900

    work

--- src/main/java/com/ll/security_2025_01_06/global/security/SecurityConfig.java
+++ src/main/java/com/ll/security_2025_01_06/global/security/SecurityConfig.java
@@ -1,16 +1,22 @@
 
 package com.ll.security_2025_01_06.global.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain baseSecurityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
@@ -31,7 +37,8 @@ public class SecurityConfig {
                 .csrf(
                         csrf ->
                                 csrf.disable()
-                );
+                )
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
